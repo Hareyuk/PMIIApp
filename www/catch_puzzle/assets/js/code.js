cac414d72c7c1e8412c57062c4246e3ce7141d13
Hareyuk <55245056+Hareyuk@users.noreply.github.com>
2019-12-07 05:59:47 -0300

    Mejorando
    Ya casi... Ya se está logrando generar ahora bien algunas matrices randoms, pero siguen siendo problema. No tienen buenos caminos, pero ahora es muy funcional el generador.

--- www/catch_puzzle/assets/js/code.js
+++ www/catch_puzzle/assets/js/code.js
@@ -66,30 +66,6 @@ function generateMaze(w, h, m)
     document.getElementById("game").appendChild(table);
 }
 
-function createWallH(posX, posI, posF, free, m)
-{
-    for(var i = posI; i <= posF; i++)
-    {
-        if(i != free)
-        {
-            m[posX][i] = "X";
-        }
-    }
-    return m;
-}
-
-function createWallV(posY, posI, posF, free, m)
-{
-    for(var i = posI; i <= posF; i++)
-    {
-        if(i!=free)
-        {
-            m[i][posY] = "X";
-        }
-    }
-    return m;
-}
-
 function genRandom(min, max)
 {
     return Math.round(Math.random() * (max - min) + min);
@@ -109,9 +85,113 @@ function markWallsMatrix(w, h, m)
         m[i][m.length-1] = "X";
     }
 
-    var arrayPositions;
+    var arrayPositions = obtainPositions(w, h);
+    while(arrayPositions.length>0)
+    {
+        var amountPos = arrayPositions.length - 1;
+        var posRandom = genRandom(0, amountPos);
+        var positions = arrayPositions[posRandom];
+        positions = positions.split("_");
+        arrayPositions.splice(posRandom, 1);
+        var posX = parseInt(positions[0]);
+        var posY = parseInt(positions[1]); 
+        var dir = genRandom(0, 3); //0 = up | 1 = left | 2 = down | 3 = right
+        while(posY > 1 && posY < h-2 && posX > 1 && posX < w-2)
+        {
+            if(m[posX][posY] == "X")
+            {
+                break;
+            }
+            if(dir == 0)
+            {
+                if(m[posX-1][posY] == "X")
+                {
+                    m[posX][posY] = null;
+                    if(posX > 3)
+                    {
+                        m[posX-2][posY] = null;
+                    }
+                    posX-=3;
+                }
+                else
+                {
+                    posX--;
+                    m[posX][posY] = "X";
+                }
+               // canPaintNext = nextMove(-1,0, posX, posY, m);
+            }
+            else if(dir == 1)
+            {
+                if(m[posX][posY+1] == "X")
+                {
+                    m[posX][posY] = null;
+                    if(posY < h-3)
+                    {
+                        m[posX][posY+2] = null;
+                    }
+                    posY+=3;
+                }
+                else
+                {
+                    posY++;
+                    m[posX][posY] = "X";
+                }
+            }
+            else if(dir == 2)
+            {
+                if(m[posX+1][posY] == "X")
+                {
+                    m[posX][posY] = null;
+                    if(posX < w-3)
+                    {
+                        m[posX+2][posY] = null;
+                    }
+                    posX+=3;
+                }
+                else
+                {
+                    posX++;
+                    m[posX][posY] = "X";
+                }
+            }
+            else if(dir == 3)
+            {
+                if(m[posX][posY-1] == "X")
+                {
+                    m[posX][posY] = null;
+                    if(posY > 3)
+                    {
+                        m[posX][posY-2] = null;
+                    }
+                    posY-=3;
+                }
+                else
+                {
+                    posY--;
+                    m[posX][posY] = "X";
+                }
+            }
+        }
+        
+    }
     
     return m;
+}
+
+function obtainPositions(w, h)
+{
+    var array = [];
+    for(var i=0;i<w;i++)
+    {
+        for(var j=0;j<h;j++)
+        {
+            if(i>1 && i < (w-2) && j > 1 && j < (h-2))
+            {
+                array.push(i+"_"+j);
+            }
+        }
+    }
+    return array;
 }
 
 function getData()
