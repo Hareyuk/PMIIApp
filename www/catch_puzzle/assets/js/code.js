3a1d08d64cd05b32dd161b9987b6df45287d05c3
Hareyuk <55245056+Hareyuk@users.noreply.github.com>
2019-12-07 06:52:38 -0300

    Laberintos generables automáticamente
    No son perfectas del todo, pero sirven para programar la jugabilidad ahora.

--- www/catch_puzzle/assets/js/code.js
+++ www/catch_puzzle/assets/js/code.js
@@ -2,8 +2,8 @@ var turn;
 var mapMatrix = [];
 var posPlayer = [];
 var gameData;
-var widthMap = 20;
-var heightMap = 20;
+var widthMap = 30;
+var heightMap = 30;
 var players;
 var playersTimes = [{points: 0, time: 0}, {points: 0, time: 0}];
 
@@ -85,7 +85,36 @@ function markWallsMatrix(w, h, m)
         m[i][m.length-1] = "X";
     }
 
+    m = buildWalls(w, h, m);
+    m = cellsLocked(w, h, m);
+    return m;
+}
+
+function cellsLocked(w, h, m)
+{
+    for(var i=1;i<h-2;i++)
+    {
+        for(var j=1;j<w-2;j++)
+        {
+            var top = m[i-1][j];
+            var left = m[i][j-1];
+            var bottom = m[i+1][j];
+            var right = m[i][j+1];
+            if(top == left && top == right && top == bottom && top == "X")
+            {
+                m[i][j] = "X";
+            }
+        }
+    }
+    return m;
+}
+
+function buildWalls(w, h, m)
+{
     var arrayPositions = obtainPositions(w, h);
+    arrayPositions = shuffle(arrayPositions);
+    arrayPositions.splice(w*h/(w+h));
+    var dir = genRandom(0, 3); //0 = up | 1 = left | 2 = down | 3 = right
     while(arrayPositions.length>0)
     {
         var amountPos = arrayPositions.length - 1;
@@ -95,13 +124,8 @@ function markWallsMatrix(w, h, m)
         arrayPositions.splice(posRandom, 1);
         var posX = parseInt(positions[0]);
         var posY = parseInt(positions[1]); 
-        var dir = genRandom(0, 3); //0 = up | 1 = left | 2 = down | 3 = right
         while(posY > 1 && posY < h-2 && posX > 1 && posX < w-2)
         {
-            if(m[posX][posY] == "X")
-            {
-                break;
-            }
             if(dir == 0)
             {
                 if(m[posX-1][posY] == "X")
@@ -117,8 +141,8 @@ function markWallsMatrix(w, h, m)
                 {
                     posX--;
                     m[posX][posY] = "X";
+                    posX--;
                 }
-               // canPaintNext = nextMove(-1,0, posX, posY, m);
             }
             else if(dir == 1)
             {
@@ -135,6 +159,7 @@ function markWallsMatrix(w, h, m)
                 {
                     posY++;
                     m[posX][posY] = "X";
+                    posY++;
                 }
             }
             else if(dir == 2)
@@ -152,6 +177,7 @@ function markWallsMatrix(w, h, m)
                 {
                     posX++;
                     m[posX][posY] = "X";
+                    posX++;
                 }
             }
             else if(dir == 3)
@@ -169,12 +195,13 @@ function markWallsMatrix(w, h, m)
                 {
                     posY--;
                     m[posX][posY] = "X";
+                    posY--;
                 }
             }
         }
+        (dir < 3) ? dir++ : dir = 0;
         
     }
-    
     return m;
 }
 
@@ -185,7 +212,7 @@ function obtainPositions(w, h)
     {
         for(var j=0;j<h;j++)
         {
-            if(i>1 && i < (w-2) && j > 1 && j < (h-2))
+            if(i>2 && i < (w-3) && j > 2 && j < (h-3))
             {
                 array.push(i+"_"+j);
             }
@@ -205,3 +232,20 @@ function saveData()
 {
 
 }
+
+//Taken from: http://sedition.com/perl/javascript-fy.html
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (0 !== currentIndex) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
