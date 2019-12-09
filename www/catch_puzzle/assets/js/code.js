db0a7c12629a5331706abc048108a43d75e0284c
Hareyuk <55245056+Hareyuk@users.noreply.github.com>
2019-12-09 03:09:32 -0300

    Laberinto éxitoso
    Ya generado el personaje y el laberinto. Ya se pudo meter el laberinto movible sólo dentro del área, aún no responsive o adaptable a tamaños de pantalla (seguramente tendrá que ser un tamaño específico para cada mínimo de ancho de pantalla).
    Probando realizar eventos con teclado (flechas) para hacer funcionar el movimiento de personaje. Si funciona, pasar a probar con evento de touch.

--- www/catch_puzzle/assets/js/code.js
+++ www/catch_puzzle/assets/js/code.js
@@ -1,11 +1,13 @@
-var turn;
+var turn = "johan";
 var mapMatrix = [];
-var posPlayer = [];
+var posPlayer = {x:0,y:0};
 var gameData;
 var widthMap = 20;
 var heightMap = 20;
 var players;
 var playersTimes = [{points: 0, time: 0}, {points: 0, time: 0}];
+var canMove = true;
+var tableGame;
 
 function startGame()
 {
@@ -24,7 +26,20 @@ function startGame()
     }*/
     mapMatrix = generateMatrix(widthMap, heightMap, mapMatrix);
     mapMatrix = markWallsMatrix(widthMap, heightMap, mapMatrix);
-    generateMaze(widthMap, heightMap, mapMatrix);
+    mapMatrix = putPlayerInMaze(widthMap, heightMap,mapMatrix);
+    mapMatrix = putPieces(widthMap,heightMap, mapMatrix);
+    generateMazeTable(widthMap, heightMap, mapMatrix);
+    moveTable();
+    generateCharacter(turn);
+}
+
+function generateCharacter(character)
+{
+    var img = document.createElement("img");
+    img.alt = character;
+    img.src = "assets/img/frames_"+character+"/1.png";
+    img.classList.add("character");
+    document.querySelector("#game div").appendChild(img);
 }
 
 function generateMatrix(w, h, m)
@@ -40,8 +55,29 @@ function generateMatrix(w, h, m)
     return m;
 }
 
-function generateMaze(w, h, m)
+function putPlayerInMaze(w, h, m)
 {
+    var randX = genRandom(1,h-2);
+    var randY = genRandom(1, w-2);
+    while(m[randX][randY] == "X")
+    {
+        randX = genRandom(1,h-2);
+        randY = genRandom(1, w-2);
+    }
+    m[randX][randY] = "P";
+    posPlayer.x = randX;
+    posPlayer.y = randY;
+    return m;
+}
+
+function putPieces(w, h, m)
+{
+    return m;
+}
+
+function generateMazeTable(w, h, m)
+{
+    var div = document.createElement("div");
     var table = document.createElement("table");
     for(var i=0;i <w;i++)
     {
@@ -49,8 +85,6 @@ function generateMaze(w, h, m)
         for(var j=0;j<h;j++)
         {
             var td = document.createElement("td");
-            td.style.width = "20px";
-            td.style.height = "20px";
             if(m[i][j] == null)
             {
                 td.classList.add("passThrough");
@@ -59,11 +93,25 @@ function generateMaze(w, h, m)
             {
                 td.classList.add("doNotPass");
             }
+            else if (m[i][j] == "P")
+            {
+                //Add turn later
+                td.classList.add("player");
+            }
             tr.appendChild(td);
         }
         table.appendChild(tr);
+        table.id = "tableGame";
     }
-    document.getElementById("game").appendChild(table);
+    div.appendChild(table);
+    document.getElementById("game").appendChild(div);
+    tableGame = document.getElementById("tableGame");
+}
+
+async function moveTable()
+{
+    tableGame.style.top = (-posPlayer.x*100+200)+"px";
+    tableGame.style.left = (-posPlayer.y*100+200)+"px";
 }
 
 function genRandom(min, max)
@@ -78,8 +126,10 @@ function markWallsMatrix(w, h, m)
     {
         //Left
         m[i][0] = "X";
+        m[i][1] = "X";
         //Right
         m[i][h-1] = "X";
+        m[i][h-2] = "X";
     }
 
     //Up & Down
@@ -87,8 +137,10 @@ function markWallsMatrix(w, h, m)
     {
         //Up
         m[0][i] = "X";
+        m[1][i] = "X";
         //Down
-        m[m.length-1][i] = "X";
+        m[w-1][i] = "X";
+        m[w-2][i] = "X";
     }
     m = buildWalls(w, h, m);
     m = cellsLocked(w, h, m);
@@ -137,6 +189,7 @@ function buildWalls(w, h, m)
                 //Found a wall in two cells top
                 if(m[posX-1][posY] == "X" && cellFree == true)
                 {
+                    
                     //Coming from other side?
                     if(m[posX-1][posY-1] == "X" && m[posX-1][posY+1] =="X")
                     {   
@@ -279,17 +332,52 @@ function saveData()
 
 //Taken from: http://sedition.com/perl/javascript-fy.html
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    while (0 !== currentIndex) {
-  
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-  
-    return array;
-  }
+var currentIndex = array.length, temporaryValue, randomIndex;
+
+while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+}
+
+return array;
+}
+
+
+/* Keyboard events from https://stackoverflow.com/questions/14261062/js-function-when-keyboard-key-is-pressed/14262201 */
+function keyListener(event){ 
+//whatever we want to do goes in this block
+event = event || window.event; //capture the event, and ensure we have an event
+var key = event.key || event.which || event.keyCode; //find the key that was pressed
+//MDN is better at this: https://developer.mozilla.org/en-US/docs/DOM/event.which
+if(key===84){ //this is for 'T'
+    doThing();
+}
+}
+
+
+/* the last example replace this one */
+
+var el = window; //we identify the element we want to target a listener on
+//remember IE can't capture on the window before IE9 on keypress.
+
+var eventName = 'keypress'; //know which one you want, this page helps you figure that out: http://www.quirksmode.org/dom/events/keys.html
+//and here's another good reference page: http://unixpapa.com/js/key.html
+//because you are looking to capture for things that produce a character
+//you want the keypress event.
+
+//we are looking to bind for IE or non-IE, 
+//so we have to test if .addEventListener is supported, 
+//and if not, assume we are on IE. 
+//If neither exists, you're screwed, but we didn't cover that in the else case.
+if (el.addEventListener) {
+el.addEventListener('click', keyListener, false); 
+} else if (el.attachEvent)  {
+el.attachEvent('on'+eventName, keyListener);
+}
+
+//and at this point you're done with registering the function, happy monitoring
